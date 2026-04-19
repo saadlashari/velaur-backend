cfd0f06c89340c09492027d116d9f805b2ff351b
Saad Ul Iman <saadlashari32@gmail.com>
2026-04-19 11:43:49 +0500

    fix install issue

---
+++ frontend/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'https://velaur-k92p.onrender.com',
+      '/media': 'https://velaur-k92p.onrender.com',
+    }
+  }
+})
